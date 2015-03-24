4743fa107cdb5d103150450c1e4f411445672f91
itsthomas <itsthomas@outlook.com>
2015-03-24 11:28:48 +0000

    initial commit

---
+++ config.rb
@@ -0,0 +1,29 @@
+# Require any additional compass plugins here.
+# require 'compass'
+require 'susy'
+# require 'breakpoint'
+# require 'sassy-buttons'
+
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'sass'
+images_dir = 'images'
+javascripts_dir = 'js'
+fonts_dir = 'fonts'
+
+#   To enable debugging comments that display the original location of your selectors.
+line_comments = true
+
+preferred_syntax = :sass
+
+sprite_engine = :chunky_png
+
+#   :expanded, :compact, :compressed
+output_style = :expanded
+
+#   Indicates whether the compass helper functions should 
+#   generate relative urls from the generated css to assets, or absolute urls using the http path for that asset type. 
+relative_assets = true
+
+# 	Enable Sass sourcemap option
+sourcemap = true;
